6ffceeb1920f41ffd0a8d6dabb68ad3381c55554
Julien Viet <julien@julienviet.com>
2015-03-17 10:25:55 +0100

    RX examples cannot be generated

--- rx-examples/src/main/groovy/io/vertx/example/rx/http/simple/server.groovy
+++
@@ -1,5 +0,0 @@
-def server = vertx.createHttpServer()
-server.requestStream().toObservable().subscribe({ req ->
-  req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>")
-})
-server.listen(8080)
--- rx-examples/src/main/js/io/vertx/example/rx/http/simple/client.js
+++
@@ -1,9 +0,0 @@
-var client = vertx.createHttpClient();
-var req = client.request('GET', 8080, "localhost", "/");
-req.toObservable().subscribe(function (resp) {
-  console.log("Got response " + resp.statusCode());
-  resp.bodyHandler(function (body) {
-    console.log("Got data " + body.toString("ISO-8859-1"));
-  });
-});
-req.end();
