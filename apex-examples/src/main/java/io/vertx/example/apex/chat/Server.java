1ff16bf2a8db0770a5c71b67f12c35e3537ffc3d
purplefox <timvolpe@gmail.com>
2015-03-24 06:18:07 +0000

    adjusted chat example

---
+++ apex-examples/src/main/java/io/vertx/example/apex/chat/Server.java
@@ -0,0 +1,62 @@
+package io.vertx.example.apex.chat;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.example.util.Runner;
+import io.vertx.ext.apex.Router;
+import io.vertx.ext.apex.handler.StaticHandler;
+import io.vertx.ext.apex.handler.sockjs.BridgeOptions;
+import io.vertx.ext.apex.handler.sockjs.PermittedOptions;
+import io.vertx.ext.apex.handler.sockjs.SockJSHandler;
+
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * A {@link io.vertx.core.Verticle} which implements a simple, realtime,
+ * multiuser chat. Anyone can connect to the chat application on port
+ * 8000 and type messages. The messages will be rebroadcast to all
+ * connected users via the @{link EventBus} Websocket bridge.
+ *
+ * @author <a href="https://github.com/InfoSec812">Deven Phillips</a>
+ */
+public class Server extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Server.class);
+  }
+
+  @Override
+  public void start() throws Exception {
+
+    Router router = Router.router(vertx);
+
+    // Allow events for the designated addresses in/out of the event bus bridge
+    BridgeOptions opts = new BridgeOptions()
+      .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
+      .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
+
+    // Create the event bus bridge and add it to the router.
+    SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
+    router.route("/eventbus/*").handler(ebHandler);
+
+    // Create a router endpoint for the static content.
+    router.route().handler(StaticHandler.create());
+
+    // Start the web server and tell it to use the router to handle requests.
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+    EventBus eb = vertx.eventBus();
+
+    // Register to listen for messages coming IN to the server
+    eb.consumer("chat.to.server").handler(message -> {
+      // Create a timestamp string
+      String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
+      // Send the message back out to all clients with the timestamp prepended.
+      eb.publish("chat.to.client", timestamp + ": " + message.body());
+    });
+
+  }
+}
