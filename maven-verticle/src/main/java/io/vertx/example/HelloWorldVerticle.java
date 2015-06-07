0e9d1917f0ae0305d32bb952346a85e45659c07f
purplefox <timvolpe@gmail.com>
2015-06-07 14:18:26 +0100

    Removed unnecessary throws Exception

--- maven-verticle/src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ maven-verticle/src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -1,12 +1,11 @@
 package io.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 
 public class HelloWorldVerticle extends AbstractVerticle {
 
   @Override
-  public void start() throws Exception {
+  public void start() {
     // Create an HTTP server which simply returns "Hello World!" to each request.
     vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
   }
