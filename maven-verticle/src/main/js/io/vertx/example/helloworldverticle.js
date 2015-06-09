6748ef3cb7a7ac0764cf898e54ddff7f6f8412c7
Julien Viet <julien@julienviet.com>
2015-06-09 18:39:13 +0200

    Remove old codetranslated examples from maven-verticle

--- maven-verticle/src/main/js/io/vertx/example/helloworldverticle.js
+++
@@ -1,4 +0,0 @@
-var Vertx = require("vertx-js/vertx");
-// Create an HTTP server which simply returns "Hello World!" to each request.
-Vertx.vertx().createHttpServer().requestHandler(function (req) {
-  req.response().end("Hello World!")}).listen(8080);
