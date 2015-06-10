35919195e83cfe101c4687f03a98367186338961
Paulo Lopes <pmlopes@gmail.com>
2015-06-10 15:29:43 +0200

    transpile angularjs

---
+++ web-examples/src/main/groovy/io/vertx/example/web/angularjs/server.groovy
@@ -0,0 +1,206 @@
+import groovy.transform.Field
+import io.vertx.groovy.ext.mongo.MongoClient
+import io.vertx.groovy.ext.web.Router
+import io.vertx.groovy.ext.web.handler.BodyHandler
+import io.vertx.groovy.ext.web.handler.StaticHandler
+@Field def mongo
+def loadData(db) {
+  db.dropCollection("users", { drop ->
+    if (drop.failed()) {
+      throw new java.lang.RuntimeException(drop.cause())
+    }
+
+    def users = new java.util.LinkedList()
+
+    users.add([
+      username:"pmlopes",
+      firstName:"Paulo",
+      lastName:"Lopes",
+      address:"The Netherlands"
+    ])
+
+    users.add([
+      username:"timfox",
+      firstName:"Tim",
+      lastName:"Fox",
+      address:"The Moon"
+    ])
+
+    users.each { user ->
+      db.insert("users", user, { res ->
+        println("inserted ${groovy.json.JsonOutput.toJson(user)}")
+      })
+    }
+  })
+}
+
+vertx.deployVerticle("maven:io.vertx:vertx-mongo-embedded-db:3.0.0-milestone6", { done ->
+  // Create a mongo client using all defaults (connect to localhost and default port) using the database name "demo".
+  mongo = MongoClient.createShared(vertx, [
+    db_name:"demo"
+  ])
+
+  // the load function just populates some data on the storage
+  this.loadData(mongo)
+
+  def router = Router.router(vertx)
+
+  router.route().handler(BodyHandler.create())
+
+  // define some REST API
+  router.get("/api/users").handler({ ctx ->
+    mongo.find("users", [:], { lookup ->
+      // error handling
+      if (lookup.failed()) {
+        ctx.fail(500)
+        return
+      }
+
+      // now convert the list to a JsonArray because it will be easier to encode the final object as the response.
+      def json = [
+      ]
+
+      lookup.result().each { o ->
+        json.add(o)
+      }
+
+      ctx.response().putHeader(io.vertx.core.http.HttpHeaders.CONTENT_TYPE, "application/json")
+      ctx.response().end(groovy.json.JsonOutput.toJson(json))
+    })
+  })
+
+  router.get("/api/users/:id").handler({ ctx ->
+    mongo.findOne("users", [
+      id:ctx.request().getParam("id")
+    ], null, { lookup ->
+      // error handling
+      if (lookup.failed()) {
+        ctx.fail(500)
+        return
+      }
+
+      def user = lookup.result()
+
+      if (user == null) {
+        ctx.fail(404)
+      } else {
+        ctx.response().putHeader(io.vertx.core.http.HttpHeaders.CONTENT_TYPE, "application/json")
+        ctx.response().end(groovy.json.JsonOutput.toJson(user))
+      }
+    })
+  })
+
+  router.post("/api/users").handler({ ctx ->
+    def newUser = ctx.getBodyAsJson()
+
+    mongo.findOne("users", [
+      username:newUser.username
+    ], null, { lookup ->
+      // error handling
+      if (lookup.failed()) {
+        ctx.fail(500)
+        return
+      }
+
+      def user = lookup.result()
+
+      if (user != null) {
+        // already exists
+        ctx.fail(500)
+      } else {
+        mongo.insert("users", newUser, { insert ->
+          // error handling
+          if (insert.failed()) {
+            ctx.fail(500)
+            return
+          }
+
+          // add the generated id to the user object
+          newUser.id = insert.result()
+
+          ctx.response().putHeader(io.vertx.core.http.HttpHeaders.CONTENT_TYPE, "application/json")
+          ctx.response().end(groovy.json.JsonOutput.toJson(newUser))
+        })
+      }
+    })
+  })
+
+  router.put("/api/users/:id").handler({ ctx ->
+    mongo.findOne("users", [
+      id:ctx.request().getParam("id")
+    ], null, { lookup ->
+      // error handling
+      if (lookup.failed()) {
+        ctx.fail(500)
+        return
+      }
+
+      def user = lookup.result()
+
+      if (user == null) {
+        // does not exist
+        ctx.fail(404)
+      } else {
+
+        // update the user properties
+        def update = ctx.getBodyAsJson()
+
+        user.username = update.username
+        user.firstName = update.firstName
+        user.lastName = update.lastName
+        user.address = update.address
+
+        mongo.replace("users", [
+          id:ctx.request().getParam("id")
+        ], user, { replace ->
+          // error handling
+          if (replace.failed()) {
+            ctx.fail(500)
+            return
+          }
+
+          ctx.response().putHeader(io.vertx.core.http.HttpHeaders.CONTENT_TYPE, "application/json")
+          ctx.response().end(groovy.json.JsonOutput.toJson(user))
+        })
+      }
+    })
+  })
+
+  router.delete("/api/users/:id").handler({ ctx ->
+    mongo.findOne("users", [
+      id:ctx.request().getParam("id")
+    ], null, { lookup ->
+      // error handling
+      if (lookup.failed()) {
+        ctx.fail(500)
+        return
+      }
+
+      def user = lookup.result()
+
+      if (user == null) {
+        // does not exist
+        ctx.fail(404)
+      } else {
+
+        mongo.remove("users", [
+          id:ctx.request().getParam("id")
+        ], { remove ->
+          // error handling
+          if (remove.failed()) {
+            ctx.fail(500)
+            return
+          }
+
+          ctx.response().setStatusCode(204)
+          ctx.response().end()
+        })
+      }
+    })
+  })
+
+  // Create a router endpoint for the static content.
+  router.route().handler(StaticHandler.create())
+
+  vertx.createHttpServer().requestHandler(router.&accept).listen(8080)
+})
--- web-examples/src/main/java/io/vertx/example/web/angularjs/Server.java
+++ web-examples/src/main/java/io/vertx/example/web/angularjs/Server.java
@@ -5,149 +5,211 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.example.util.Runner;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * @author <a href="mailto:pmlopes@gmail.com">Paulo Lopes</a>
  */
 public class Server extends AbstractVerticle {
 
-  // Fake DB
-  private static final Map<String, JsonObject> DB = new ConcurrentHashMap<>();
-
-  static {
-
-    insert(new JsonObject()
-      .put("username", "pmlopes")
-      .put("firstName", "Paulo")
-      .put("lastName", "Lopes")
-      .put("address", "The Netherlands"));
-
-    insert(new JsonObject()
-      .put("username", "timfox")
-      .put("firstName", "Tim")
-      .put("lastName", "Fox")
-      .put("address", "The Moon"));
-  }
-
-  public static JsonArray findAll() {
-    JsonArray l = new JsonArray();
-
-    for (JsonObject u : DB.values()) {
-      l.add(u);
-    }
-
-    return l;
-  }
-
-  public static JsonObject findById(String id) {
-    return DB.get(id);
-  }
-
-  public static JsonObject findByUsername(String username) {
-    for (JsonObject u : DB.values()) {
-      if (u.getString("username", "").equals(username)) {
-        return u;
-      }
-    }
-
-    return null;
-  }
-
-  public static JsonObject insert(JsonObject u) {
-    String id = UUID.randomUUID().toString();
-    u.put("id", id);
-    DB.put(id, u);
-    return u;
-  }
-
-  public static JsonObject update(JsonObject u) {
-    DB.put(u.getString("id"), u);
-    return u;
-  }
-
-  public static void remove(String id) {
-    DB.remove(id);
-  }
-
-
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
     Runner.runExample(Server.class);
   }
 
+  private MongoClient mongo;
+
   @Override
   public void start() throws Exception {
 
-    Router router = Router.router(vertx);
+    vertx.deployVerticle("maven:io.vertx:vertx-mongo-embedded-db:3.0.0-milestone6", done -> {
+      // Create a mongo client using all defaults (connect to localhost and default port) using the database name "demo".
+      mongo = MongoClient.createShared(vertx, new JsonObject().put("db_name", "demo"));
 
-    router.route().handler(BodyHandler.create());
+      // the load function just populates some data on the storage
+      loadData(mongo);
 
-    // define some REST API
-    router.get("/api/users").handler(ctx -> {
-      ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-      ctx.response().end(findAll().encode());
+      Router router = Router.router(vertx);
+
+      router.route().handler(BodyHandler.create());
+
+      // define some REST API
+      router.get("/api/users").handler(ctx -> {
+        mongo.find("users", new JsonObject(), lookup -> {
+          // error handling
+          if (lookup.failed()) {
+            ctx.fail(500);
+            return;
+          }
+
+          // now convert the list to a JsonArray because it will be easier to encode the final object as the response.
+          final JsonArray json = new JsonArray();
+
+          for (JsonObject o : lookup.result()) {
+            json.add(o);
+          }
+
+          ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+          ctx.response().end(json.encode());
+        });
+      });
+
+      router.get("/api/users/:id").handler(ctx -> {
+        mongo.findOne("users", new JsonObject().put("id", ctx.request().getParam("id")), null, lookup -> {
+          // error handling
+          if (lookup.failed()) {
+            ctx.fail(500);
+            return;
+          }
+
+          JsonObject user = lookup.result();
+
+          if (user == null) {
+            ctx.fail(404);
+          } else {
+            ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            ctx.response().end(user.encode());
+          }
+        });
+      });
+
+      router.post("/api/users").handler(ctx -> {
+        JsonObject newUser = ctx.getBodyAsJson();
+
+        mongo.findOne("users", new JsonObject().put("username", newUser.getString("username")), null, lookup -> {
+          // error handling
+          if (lookup.failed()) {
+            ctx.fail(500);
+            return;
+          }
+
+          JsonObject user = lookup.result();
+
+          if (user != null) {
+            // already exists
+            ctx.fail(500);
+          } else {
+            mongo.insert("users", newUser, insert -> {
+              // error handling
+              if (insert.failed()) {
+                ctx.fail(500);
+                return;
+              }
+
+              // add the generated id to the user object
+              newUser.put("id", insert.result());
+
+              ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+              ctx.response().end(newUser.encode());
+            });
+          }
+        });
+      });
+
+      router.put("/api/users/:id").handler(ctx -> {
+        mongo.findOne("users", new JsonObject().put("id", ctx.request().getParam("id")), null, lookup -> {
+          // error handling
+          if (lookup.failed()) {
+            ctx.fail(500);
+            return;
+          }
+
+          JsonObject user = lookup.result();
+
+          if (user == null) {
+            // does not exist
+            ctx.fail(404);
+          } else {
+
+            // update the user properties
+            JsonObject update = ctx.getBodyAsJson();
+
+            user.put("username", update.getString("username"));
+            user.put("firstName", update.getString("firstName"));
+            user.put("lastName", update.getString("lastName"));
+            user.put("address", update.getString("address"));
+
+            mongo.replace("users", new JsonObject().put("id", ctx.request().getParam("id")), user, replace -> {
+              // error handling
+              if (replace.failed()) {
+                ctx.fail(500);
+                return;
+              }
+
+              ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+              ctx.response().end(user.encode());
+            });
+          }
+        });
+      });
+
+      router.delete("/api/users/:id").handler(ctx -> {
+        mongo.findOne("users", new JsonObject().put("id", ctx.request().getParam("id")), null, lookup -> {
+          // error handling
+          if (lookup.failed()) {
+            ctx.fail(500);
+            return;
+          }
+
+          JsonObject user = lookup.result();
+
+          if (user == null) {
+            // does not exist
+            ctx.fail(404);
+          } else {
+
+            mongo.remove("users", new JsonObject().put("id", ctx.request().getParam("id")), remove -> {
+              // error handling
+              if (remove.failed()) {
+                ctx.fail(500);
+                return;
+              }
+
+              ctx.response().setStatusCode(204);
+              ctx.response().end();
+            });
+          }
+        });
+      });
+
+      // Create a router endpoint for the static content.
+      router.route().handler(StaticHandler.create());
+
+      vertx.createHttpServer().requestHandler(router::accept).listen(8080);
     });
+  }
 
-    router.get("/api/users/:id").handler(ctx -> {
-      JsonObject user = findById(ctx.request().getParam("id"));
+  private void loadData(MongoClient db) {
+    db.dropCollection("users", drop -> {
+      if (drop.failed()) {
+        throw new RuntimeException(drop.cause());
+      }
 
-      if (user == null) {
-        ctx.fail(404);
-      } else {
-        ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        ctx.response().end(user.encode());
+      List<JsonObject> users = new LinkedList<>();
+
+      users.add(new JsonObject()
+              .put("username", "pmlopes")
+              .put("firstName", "Paulo")
+              .put("lastName", "Lopes")
+              .put("address", "The Netherlands"));
+
+      users.add(new JsonObject()
+              .put("username", "timfox")
+              .put("firstName", "Tim")
+              .put("lastName", "Fox")
+              .put("address", "The Moon"));
+
+      for (JsonObject user : users) {
+        db.insert("users", user, res -> {
+          System.out.println("inserted " + user.encode());
+        });
       }
     });
-
-    router.post("/api/users").handler(ctx -> {
-      JsonObject user = findByUsername(ctx.getBodyAsJson().getString("username"));
-
-      if (user != null) {
-        // already exists
-        ctx.fail(500);
-      } else {
-        ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        user = insert(ctx.getBodyAsJson());
-
-        ctx.response().end(user.encode());
-      }
-    });
-
-    router.put("/api/users/:id").handler(ctx -> {
-      JsonObject user = findById(ctx.request().getParam("id"));
-
-      if (user == null) {
-        ctx.fail(404);
-      } else {
-        user = update(user.mergeIn(ctx.getBodyAsJson()));
-
-        ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        ctx.response().end(user.encode());
-      }
-    });
-
-    router.delete("/api/users/:id").handler(ctx -> {
-      JsonObject user = findById(ctx.request().getParam("id"));
-
-      if (user == null) {
-        ctx.fail(404);
-      } else {
-        remove(ctx.request().getParam("id"));
-        ctx.response().setStatusCode(204);
-        ctx.response().end();
-      }
-    });
-
-    // Create a router endpoint for the static content.
-    router.route().handler(StaticHandler.create());
-
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
   }
 }
