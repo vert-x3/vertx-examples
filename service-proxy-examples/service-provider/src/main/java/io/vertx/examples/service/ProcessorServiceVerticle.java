f41e5e6eebce98661e9f995a610b0efd170c2ed5
Clement Escoffier <clement.escoffier@gmail.com>
2015-06-20 16:24:06 +0200

    Replace generated classes usages by the ProxyHelper.
    Update the documentation to mention the two ways,

--- service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/ProcessorServiceVerticle.java
+++ service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/ProcessorServiceVerticle.java
@@ -2,6 +2,7 @@ package io.vertx.examples.service;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.examples.service.impl.ProcessorServiceImpl;
+import io.vertx.serviceproxy.ProxyHelper;
 
 /**
  * The verticle publishing the service.
@@ -15,8 +16,7 @@ public class ProcessorServiceVerticle extends AbstractVerticle {
     // Create the client object
     service = new ProcessorServiceImpl();
     // Register the handler
-    new ProcessorServiceVertxProxyHandler(vertx, service)
-        .registerHandler("vertx.processor");
+    ProxyHelper.registerService(ProcessorService.class, vertx, service, "vertx.processor");
   }
 
 }
