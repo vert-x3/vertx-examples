4f8cd9536500ee916fdc804946f980203a2b0414
Clement Escoffier <clement.escoffier@gmail.com>
2015-06-21 10:42:46 +0200

    #54 - Remove a reference to generated classes
    As the classes are generated at compile time when compiling with Maven, IDEs cannot find them.

--- service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/ProcessorService.java
+++ service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/ProcessorService.java
@@ -7,6 +7,7 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.examples.service.impl.ProcessorServiceImpl;
+import io.vertx.serviceproxy.ProxyHelper;
 
 /**
  * The service interface.
@@ -21,9 +22,11 @@ public interface ProcessorService {
   }
 
   static ProcessorService createProxy(Vertx vertx, String address) {
+    return ProxyHelper.createProxy(ProcessorService.class, vertx, address);
+    // Alternatively, you can create the proxy directly using:
+    // return new ProcessorServiceVertxEBProxy(vertx, address);
     // The name of the class to instantiate is the service interface + `VertxEBProxy`.
     // This class is generated during the compilation
-    return new ProcessorServiceVertxEBProxy(vertx, address);
   }
 
   // The service methods
