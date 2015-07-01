074b44df8404c142c9289c9ef9b9446589f838da
purplefox <timvolpe@gmail.com>
2015-07-01 15:14:29 +0100

    Updated versions to 3.1.0-SNAPSHOT

--- jdbc-examples/src/main/java/io/vertx/example/query_sync/JDBCExampleSync.java
+++ jdbc-examples/src/main/java/io/vertx/example/query_sync/JDBCExampleSync.java
@@ -1,42 +1,49 @@
 package io.vertx.example.query_sync;
 
-import io.vertx.core.AbstractVerticle;
+import co.paralleluniverse.fibers.Suspendable;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.example.util.Runner;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.ResultSet;
-import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.sync.SyncVerticle;
 import io.vertx.sync.ext.jdbc.JDBCClientSync;
 import io.vertx.sync.ext.sql.SQLConnectionSync;
 
 /*
- * @author <a href="mailto:pmlopes@gmail.com">Paulo Lopes</a>
+ * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class JDBCExampleSync extends AbstractVerticle {
+public class JDBCExampleSync extends SyncVerticle {
 
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
     Runner.runExample(JDBCExampleSync.class);
   }
 
+  // Note - start method MUST be marked as Suspendable
   @Override
-  public void start() throws Exception {
+  @Suspendable
+  public void start() {
 
-    final JDBCClient client = JDBCClient.createShared(vertx, new JsonObject()
+    JDBCClient client = JDBCClient.createShared(vertx, new JsonObject()
         .put("url", "jdbc:hsqldb:mem:test?shutdown=true")
         .put("driver_class", "org.hsqldb.jdbcDriver")
         .put("max_pool_size", 30));
 
     JDBCClientSync clientSync = new JDBCClientSync(client);
-    SQLConnection conn = clientSync.getConnectionSync();
-    SQLConnectionSync syncConnection = new SQLConnectionSync(conn);
-    syncConnection.executeSync("create table test(id int primary key, name varchar(255))");
-    syncConnection.executeSync("insert into test values (1, 'Hello'), (2, 'World')");
-    ResultSet rs = syncConnection.queryWithParamsSync("select * from test where id = ?", new JsonArray().add(2));
-    for (JsonArray line : rs.getResults()) {
-      System.out.println(line.encode());
+    SQLConnectionSync conn = clientSync.getConnectionSync();
+    System.out.println("Got connection!");
+    try {
+      conn.executeSync("create table test(id int primary key, name varchar(255))");
+      System.out.println("Executed 1!");
+      conn.executeSync("insert into test values (1, 'Hello'), (2, 'World')");
+      System.out.println("Executed 2!");
+      ResultSet rs = conn.queryWithParamsSync("select * from test where id = ?", new JsonArray().add(2));
+      for (JsonArray line : rs.getResults()) {
+        System.out.println(line.encode());
+      }
+    } finally {
+      conn.closeSync();
     }
-    syncConnection.closeSync();
   }
 }
