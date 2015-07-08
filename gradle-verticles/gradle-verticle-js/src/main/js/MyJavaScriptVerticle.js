cf49bb0642cbde37acfa081c2ab37bd33ebeef0b
Clement Escoffier <clement.escoffier@gmail.com>
2015-07-08 11:07:40 +0200

    Fix #71
    Provide examples illustrating how to build fat jar from verticles implemented in different languages. Examples are provided using Gradle and Maven.
    Also reuse the version variable in the dependencies (the the grade build).

---
+++ gradle-verticles/gradle-verticle-js/src/main/js/MyJavaScriptVerticle.js
@@ -0,0 +1,3 @@
+vertx.createHttpServer().requestHandler(function (req) {
+    req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>");
+}).listen(8080);
