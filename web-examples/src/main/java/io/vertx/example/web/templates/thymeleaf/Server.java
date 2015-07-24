bc125db463edfa7091d1e105efdc73742bd42d83
Paulo Lopes <paulo@mlopes.net>
2015-07-24 10:59:57 +0200

    #75 thymeleaf example

---
+++ web-examples/src/main/java/io/vertx/example/web/templates/thymeleaf/Server.java
@@ -0,0 +1,54 @@
+package io.vertx.example.web.templates.thymeleaf;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.example.util.Runner;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
+
+/**
+ * This is an example application to showcase the usage of Vert.x Web.
+ *
+ * In this application you will see the usage of:
+ *
+ *  * Thymeleaf templates
+ *  * Vert.x Web
+ *
+ * @author <a href="mailto:pmlopes@gmail.com>Paulo Lopes</a>
+ */
+public class Server extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Server.class);
+  }
+
+  @Override
+  public void start() throws Exception {
+
+    // In order to use a JADE template we first need to create an engine
+    final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+
+    // To simplify the development of the web components we use a Router to route all HTTP requests
+    // to organize our code in a reusable way.
+    final Router router = Router.router(vertx);
+
+    // Entry point to the application, this will render a custom JADE template.
+    router.get().handler(ctx -> {
+      // we define a hardcoded title for our application
+      ctx.put("welcome", "Hi there!");
+
+      // and now delegate to the engine to render it.
+      engine.render(ctx, "templates/index.html", res -> {
+        if (res.succeeded()) {
+          ctx.response().putHeader(HttpHeaders.CONTENT_TYPE, "text/html").end(res.result());
+        } else {
+          ctx.fail(res.cause());
+        }
+      });
+    });
+
+   // start a HTTP web server on port 8080
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
+}
