448790e09cc783ee60468bff0f916ecad74e8c52
Vitor Freitas <vitor.tfreitas@gmail.com>
2015-09-06 09:07:29 -0300

    Fixing groovy and js impls

--- mongo-examples/src/main/groovy/io/vertx/examples/mongo/mongo_client_verticle.groovy
+++ mongo-examples/src/main/groovy/io/vertx/examples/mongo/mongo_client_verticle.groovy
@@ -1,19 +1,19 @@
-import io.vertx.core.Vertx
 import io.vertx.groovy.ext.mongo.MongoClient
+import io.vertx.groovy.core.Vertx
 
 config = Vertx.currentContext().config()
-uri = config.getString("mongo.uri", System.getProperty("mongo.uri", "mongodb://localhost:27017"))
-db = config.getString("mongo.db", System.getProperty("mongo.db", "test"))
+uri = config["mongo.uri"] ?: "mongodb://localhost:27017"
+db = config["mongo.db"] ?: "test"
 
 mongoClient = MongoClient.createShared(vertx, ["connection_string": uri, "db_name": db])
 
-product = ["itemId":"12345", "name": "Cooler", "price":100.0]
+product = ["itemId":"12345", "name": "Cooler", "price":"100.0"]
 
 mongoClient.save("products", product, { id ->
   println "Inserted id: ${id.result()}"
 
   mongoClient.find("products", ["itemId":"12345"],  { res ->
-    println "Product Name is ${res.result()[0].getString("name")}"
+    println "Product Name is ${res.result[0]["name"]}"
 
     mongoClient.remove("products", ["itemId":"12345"], { rs ->
       if (rs.succeeded()){
--- mongo-examples/src/main/js/io/vertx/examples/mongo/mongo_client_verticle.js
+++ mongo-examples/src/main/js/io/vertx/examples/mongo/mongo_client_verticle.js
@@ -1,21 +1,21 @@
 var MongoClient = require("vertx-mongo-js/mongo_client");
 
 var config = Java.type("io.vertx.groovy.core.Vertx").currentContext().config
-var uri = config.getString("mongo.uri", "mongodb://localhost:27017")
-var db = config.getString("mongo.db", "test")
+var uri = config["mongo.uri"] != null ? config["mongo.uri"] : "mongodb://localhost:27017"
+var db = config["mongo.db"] != null ? config["mongo.db"] : "test"
 
-client = MongoClient.createShared(vertx, { "connection_string": uri, "db_name": db})
+var client = MongoClient.createShared(vertx, { "connection_string": uri, "db_name": db})
 
-product = {"itemId":"12345", "name": "Cooler", "price":100.0}
+var product = {"itemId":"12345", "name": "Cooler", "price":"100.0"}
 
-client.save("products", product, function(id) {
-  console.log("Inserted id: " + id.result())
+client.save("products", product, function(id, ins_err) {
+  console.log("Inserted id: " + id)
 
-  client.find("products", {"itemId":"12345"},  function(res) {
-    console.log("Name is " + res.result().get(0).getString("name"))
+  client.find("products", {"itemId":"12345"},  function(f_result, fin_err) {
+    console.log("Name is " + f_result[0]["name"])
 
-    client.remove("products", {"itemId":"12345"}, function(rs) {
-      if (rs.succeeded()){
+    client.remove("products", {"itemId":"12345"}, function(d_result, del_err) {
+      if (!del_err){
         console.log("Product removed ")
       }
     })
