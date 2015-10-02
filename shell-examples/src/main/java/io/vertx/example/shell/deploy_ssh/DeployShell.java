698e1d3bf847808efc975961a27e8345474ab1c2
Julien Viet <julien@julienviet.com>
2015-10-02 18:41:17 +0200

    Added example that deploy a shell as a service

---
+++ shell-examples/src/main/java/io/vertx/example/shell/deploy_ssh/DeployShell.java
@@ -0,0 +1,41 @@
+package io.vertx.example.shell.deploy_ssh;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.example.util.Runner;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class DeployShell extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(DeployShell.class);
+  }
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    JsonObject options = new JsonObject().put("sshOptions",
+        new JsonObject().
+            put("host", "localhost").
+            put("port", 3000).
+            put("keyPairOptions", new JsonObject().
+                put("path", "keystore.jks").
+                put("password", "wibble")).
+            put("shiroAuthOptions", new JsonObject().put("config",
+                    new JsonObject().put("properties_path", "auth.properties"))
+
+            )
+    );
+    vertx.deployVerticle("service:io.vertx.ext.shell", new DeploymentOptions().setConfig(options), ar -> {
+      if (ar.succeeded()) {
+        startFuture.succeeded();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+}
---
+++ shell-examples/src/main/java/io/vertx/example/shell/deploy_telnet/DeployShell.java
@@ -0,0 +1,34 @@
+package io.vertx.example.shell.deploy_telnet;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.example.util.Runner;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class DeployShell extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(DeployShell.class);
+  }
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    JsonObject options = new JsonObject().put("telnetOptions",
+        new JsonObject().
+            put("host", "localhost").
+            put("port", 3000)
+    );
+    vertx.deployVerticle("service:io.vertx.ext.shell", new DeploymentOptions().setConfig(options), ar -> {
+      if (ar.succeeded()) {
+        startFuture.succeeded();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+}
