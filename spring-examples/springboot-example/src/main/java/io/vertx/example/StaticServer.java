74cec831f5e4db71e9400782e2a0589ea68ab428
Ian Chan <icha024@gmail.com>
2015-11-29 02:52:40 +0000

    Updated Spring Boot example to run server via Verticle

--- spring-examples/springboot-example/src/main/java/io/vertx/example/StaticServer.java
+++ spring-examples/springboot-example/src/main/java/io/vertx/example/StaticServer.java
@@ -1,28 +1,15 @@
 package io.vertx.example;
 
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
-/**
- * Created by rworsnop on 8/31/15.
- */
 @Component
-public class StaticServer {
+public class StaticServer extends AbstractVerticle {
 
-    private Vertx vertx;
-
-    @Resource
-    public void setVertx(Vertx vertx) {
-        this.vertx = vertx;
-    }
-
-    @PostConstruct
-    public void createServer(){
+    @Override
+    public void start() throws Exception {
         Router router = Router.router(vertx);
 
         // Serve the static pages
