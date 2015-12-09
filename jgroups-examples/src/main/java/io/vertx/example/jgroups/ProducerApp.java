68002e41c1fca90f89d0be00305e9bac17c60d1a
Clement Escoffier <clement.escoffier@gmail.com>
2015-12-09 15:39:42 +0100

    Add examples illustrating the jgroups cluster manager.

---
+++ jgroups-examples/src/main/java/io/vertx/example/jgroups/ProducerApp.java
@@ -0,0 +1,32 @@
+package io.vertx.example.jgroups;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+/**
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ */
+public class ProducerApp {
+
+  public static void main(String[] args) {
+    String ip = NetworkUtils.getInterface();
+    if (args.length == 1) {
+      ip = args[0];
+    }
+    System.out.println("Using interface: " + ip);
+
+    System.setProperty("jgroups.bind_addr", ip);
+    Vertx.clusteredVertx(new VertxOptions().setClustered(true).setClusterHost(ip), ar -> {
+      if (ar.failed()) {
+        System.err.println("Cannot create vert.x instance : " + ar.cause());
+      } else {
+        Vertx vertx = ar.result();
+        vertx.setPeriodic(3000, x -> {
+          System.out.println("Sending data to 'news'");
+          vertx.eventBus().send("news", "hello vert.x");
+        });
+      }
+    });
+  }
+
+}
