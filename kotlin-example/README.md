ce162eb549b872b08eb0c3a6e63ae4e3f2f3b607
Mike Gouline <mike@gouline.net>
2016-04-26 00:30:46 +1000

    Documentation and mock data

--- kotlin-example/README.md
+++ kotlin-example/README.md
@@ -1,3 +1,50 @@
 # Vert.x Kotlin Example
 
-Vert.x example application
+Basic JSON API written in [Kotlin](https://kotlinlang.org/) to demonstrate how it can be used with Vert.x.
+
+This project is build on Gradle and is ready to be deployed on Heroku, hence it combines parts of the following examples:
+
+- Gradle:
+    - [gradle_redeploy](https://github.com/vert-x3/vertx-examples/tree/master/gradle-redeploy)
+    - [gradle_simplest](https://github.com/vert-x3/vertx-examples/tree/master/gradle-simplest)
+    - [gradle_verticles](https://github.com/vert-x3/vertx-examples/tree/master/gradle-verticles)
+- Heroku:
+    - [heroku-example](https://github.com/vert-x3/vertx-examples/tree/master/heroku-example)
+
+## Running
+
+The most convenient way to run the project during development is as follows:
+
+```
+./gradlew run
+```
+
+That way, any changes to the classes will be picked up and re-deployed automatically.
+
+## Deploying
+
+The application can either be deployed manually (with a jar file) or to [Heroku](https://www.heroku.com/).
+
+### Manually
+
+You can generate a single "shadow jar" (more on that [here](https://github.com/johnrengelman/shadow)) as follows:
+
+```
+./gradlew shadowJar
+```
+
+The jar file can now be found in `./build/libs/app-shadow.jar` and deployed on your custom platform.
+
+### Heroku
+
+To deploy to Heroku, simply create an account, install the [Heroku Toolbelt](https://toolbelt.heroku.com/) and run the following:
+
+```
+heroku login
+heroku create
+git push heroku master
+```
+
+*Note:* All your changes should be committed to Git before issuing the last command.
+ 
+The `Procfile` required for Heroku deployments is included in the project.
--- kotlin-example/src/main/java/io/vertx/example/MainVerticle.kt
+++ kotlin-example/src/main/java/io/vertx/example/MainVerticle.kt
@@ -2,7 +2,11 @@ package io.vertx.example
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.Handler
+import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.Json
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
 @Suppress("unused")
 class MainVerticle : AbstractVerticle() {
@@ -12,7 +16,7 @@ class MainVerticle : AbstractVerticle() {
 
         vertx.createHttpServer()
                 .requestHandler { router.accept(it) }
-                .listen(getPort(), getHost()) { result ->
+                .listen(config().getInteger("http.port", 8080)) { result ->
                     if (result.succeeded()) {
                         startFuture?.complete()
                     } else {
@@ -22,15 +26,45 @@ class MainVerticle : AbstractVerticle() {
     }
 
     private fun createRouter() = Router.router(vertx).apply {
-        get("/").handler { req ->
-            req.response().end("Hello World!")
-        }
-        get("/test").handler { req ->
-            req.response().end("Test")
-        }
+        get("/").handler(handlerRoot)
+        get("/islands").handler(handlerIslands)
+        get("/countries").handler(handlerCountries)
     }
 
-    private fun getPort() = config().getInteger("http.port", 8080)
+    //
+    // Handlers
 
-    private fun getHost() = config().getString("http.address", "0.0.0.0")
+    val handlerRoot = Handler<RoutingContext> { req ->
+        req.response().end("Welcome!")
+    }
+
+    val handlerIslands = Handler<RoutingContext> { req ->
+        req.response().endWithJson(MOCK_ISLANDS)
+    }
+
+    val handlerCountries = Handler<RoutingContext> { req ->
+        req.response().endWithJson(MOCK_ISLANDS.map { it.country }.distinct().sortedBy { it.code })
+    }
+
+    //
+    // Mock data
+
+    private val MOCK_ISLANDS by lazy {
+        listOf(
+                Island("Kotlin", Country("Russia", "RU")),
+                Island("Stewart Island", Country("New Zealand", "NZ")),
+                Island("Cockatoo Island", Country("Australia", "AU")),
+                Island("Tasmania", Country("Australia", "AU"))
+        )
+    }
+
+    //
+    // Utilities
+
+    /**
+     * Extension to the HTTP response to output JSON objects.
+     */
+    fun HttpServerResponse.endWithJson(obj: Any) {
+        this.putHeader("Content-Type", "application/json; charset=utf-8").end(Json.encodePrettily(obj))
+    }
 }
