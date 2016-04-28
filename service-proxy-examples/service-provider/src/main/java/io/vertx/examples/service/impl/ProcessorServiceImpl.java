e68579f9c65a3ef8ddaf676d6e298dcc886c0108
Dan O'Reilly <oreilldf@gmail.com>
2016-04-27 20:21:59 -0400

    Add ServiceException to service-proxy example
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

--- service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/impl/ProcessorServiceImpl.java
+++ service-proxy-examples/service-provider/src/main/java/io/vertx/examples/service/impl/ProcessorServiceImpl.java
@@ -5,21 +5,32 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.serviceproxy.ServiceException;
 import io.vertx.examples.service.ProcessorService;
 
 /**
  * Service implementation.
  * <p/>
- * Just add `approved` to the given document.
+ * Just add `approved` to the given document, unless its missing the
+ * 'name' key, in which case an error is returned.
  */
 public class ProcessorServiceImpl implements ProcessorService {
+  public static int NO_NAME_ERROR = 2;
+  public static int BAD_NAME_ERROR = 3;
 
   @Override
   public void process(JsonObject document, Handler<AsyncResult<JsonObject>> resultHandler) {
     System.out.println("Processing...");
     JsonObject result = document.copy();
-    result.put("approved", true);
-    resultHandler.handle(Future.succeededFuture(result));
+    if (!document.containsKey("name")) {
+      resultHandler.handle(ServiceException.fail(NO_NAME_ERROR, "No name in the document"));
+    } else if (document.getString("name").isEmpty()) {
+      resultHandler.handle(ServiceException.fail(BAD_NAME_ERROR, "Bad name in the document: " +
+        document.getString("name"), new JsonObject().put("name", document.getString("name"))));
+    } else {
+      result.put("approved", true);
+      resultHandler.handle(Future.succeededFuture(result));
+    }
   }
 
 }
