ce6726a35afc76bd8ddfdccca923442d3fb71b25
Julien Viet <julien@julienviet.com>
2016-05-13 15:18:20 +0200

    HTTP2 core example with OpenSSL

---
+++ core-examples/src/main/js/io/vertx/example/core/http2/simple/client.js
@@ -0,0 +1,17 @@
+
+// Note! in real-life you wouldn't often set trust all to true as it could leave you open to man in the middle attacks.
+
+var options = {
+  "ssl" : true,
+  "useAlpn" : true,
+  "sslEngine" : 'OPENSSL',
+  "protocolVersion" : 'HTTP_2',
+  "trustAll" : true
+};
+
+vertx.createHttpClient(options).getNow(8443, "localhost", "/", function (resp) {
+  console.log("Got response " + resp.statusCode());
+  resp.bodyHandler(function (body) {
+    console.log("Got data " + body.toString("ISO-8859-1"));
+  });
+});
