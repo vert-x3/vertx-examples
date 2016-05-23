7e67535239ff41cc611e2bf3c925b31ef68dac17
Julien Viet <julien@julienviet.com>
2016-05-23 11:14:11 +0200

    HTTP2 push example

---
+++ core-examples/src/main/java/io/vertx/example/core/http2/push/Client.java
@@ -0,0 +1,50 @@
+package io.vertx.example.core.http2.push;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpVersion;
+import io.vertx.core.net.SSLEngine;
+import io.vertx.example.util.Runner;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class Client extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Client.class);
+  }
+
+  @Override
+  public void start() throws Exception {
+
+    // Note! in real-life you wouldn't often set trust all to true as it could leave you open to man in the middle attacks.
+
+    HttpClientOptions options = new HttpClientOptions().
+        setSsl(true).
+        setUseAlpn(true).
+        setSslEngine(SSLEngine.OPENSSL).
+        setProtocolVersion(HttpVersion.HTTP_2).
+        setTrustAll(true);
+
+    HttpClient client = vertx.createHttpClient(options);
+
+    HttpClientRequest request = client.get(8443, "localhost", "/", resp -> {
+      System.out.println("Got response " + resp.statusCode());
+      resp.bodyHandler(body -> System.out.println("Got data " + body.toString("ISO-8859-1")));
+    });
+
+    // Set handler for server side push
+    request.pushHandler(pushedReq -> {
+      System.out.println("Receiving pushed content");
+      pushedReq.handler(pushedResp -> {
+        pushedResp.bodyHandler(body -> System.out.println("Got pushed data " + body.toString("ISO-8859-1")));
+      });
+    });
+
+    request.end();
+  }
+}
