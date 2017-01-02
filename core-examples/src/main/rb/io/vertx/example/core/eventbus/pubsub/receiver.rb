5574ef380e1208419c9da7d2c72b08004041d91b
Thomas Segismont <tsegismo@redhat.com>
2017-01-02 15:39:07 +0100

    Update generated files

--- core-examples/src/main/rb/io/vertx/example/core/eventbus/pubsub/receiver.rb
+++ core-examples/src/main/rb/io/vertx/example/core/eventbus/pubsub/receiver.rb
@@ -2,7 +2,15 @@
 eb = $vertx.event_bus()
 
 eb.consumer("news-feed") { |message|
-  puts "Received news: #{message.body()}"
+  puts "Received news on consumer 1: #{message.body()}"
+}
+
+eb.consumer("news-feed") { |message|
+  puts "Received news on consumer 2: #{message.body()}"
+}
+
+eb.consumer("news-feed") { |message|
+  puts "Received news on consumer 3: #{message.body()}"
 }
 
 puts "Ready!"
