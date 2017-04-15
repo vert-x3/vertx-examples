e05239015704011b7cf641148c036fb04e545f63
Clement Escoffier <clement.escoffier@gmail.com>
2017-04-15 10:00:17 +0200

    Bump version to 3.4.2-SNAPSHOT

--- mail-examples/src/main/groovy/io/vertx/example/mail/mail_headers.groovy
+++ mail-examples/src/main/groovy/io/vertx/example/mail/mail_headers.groovy
@@ -13,7 +13,7 @@ def email = [
   from:"user1@example.com",
   to:["user2@example.com", "user3@example.com", "user4@example.com"],
   headers:[
-    'X-Mailer':"Vert.x Mail-Client 3.4.1",
+    'X-Mailer':"Vert.x Mail-Client 3.4.2-SNAPSHOT",
     'Message-ID':"12345@example.com",
     'Reply-To':"reply@example.com",
     Received:[
