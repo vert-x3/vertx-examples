62380c6068c9f3bc1572c2f6e9df350b768f5a75
Galder Zamarreño <galder@zamarreno.com>
2017-05-09 10:23:43 +0200

    Add Java Vert.x event bus SockJS bridge example
    * Add Vert.x event bus SockJS bridge example where the client is a Java
      client using Vert.x HTTP client API to receive events over websockets.

---
+++ web-examples/src/main/java/io/vertx/example/web/vertxbus/java/Client.java
@@ -0,0 +1,39 @@
+package io.vertx.example.web.vertxbus.java;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.WebSocket;
+import io.vertx.core.json.JsonObject;
+
+public class Client {
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    HttpClient client = vertx.createHttpClient();
+
+    client.websocket(8080, "localhost", "/eventbus/websocket", ws -> {
+      System.out.println("Connected");
+      sendPing(ws);
+
+      vertx.setPeriodic(5000, id -> {
+        sendPing(ws);
+      });
+
+      // Register
+      JsonObject msg = new JsonObject().put("type", "register").put("address", "feed");
+      ws.writeFrame(io.vertx.core.http.WebSocketFrame.textFrame(msg.encode(), true));
+
+      ws.handler(buff -> {
+        System.out.println(buff);
+      });
+    }, fail -> {
+      System.out.println("Failed: " + fail);
+    });
+  }
+
+  static void sendPing(WebSocket ws) {
+    JsonObject msg = new JsonObject().put("type", "ping");
+    ws.writeFrame(io.vertx.core.http.WebSocketFrame.textFrame(msg.encode(), true));
+  }
+
+}
