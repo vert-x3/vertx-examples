464ceaa85a2e399a376317d437d2e1685eaa84d8
Julien Viet <julien@julienviet.com>
2017-08-01 17:28:30 +0200

    Renamed for mqtt changes

---
+++ mqtt-examples/src/main/js/io/vertx/example/mqtt/server/ssl/server.js
@@ -0,0 +1,29 @@
+var MqttServer = require("vertx-mqtt-server-js/mqtt_server");
+
+var options = {
+  "port" : 8883,
+  "pemKeyCertOptions" : {
+    "keyPath" : "server-key.pem",
+    "certPath" : "server-cert.pem"
+  },
+  "ssl" : true
+};
+
+var mqttServer = MqttServer.create(vertx, options);
+
+mqttServer.endpointHandler(function (endpoint) {
+
+  // shows main connect info
+  console.log("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = " + endpoint.isCleanSession());
+
+  // accept connection from the remote client
+  endpoint.accept(false);
+
+}).listen(function (ar, ar_err) {
+
+  if (ar_err == null) {
+    console.log("MQTT server is listening on port " + mqttServer.actualPort());
+  } else {
+    console.error("Error on starting the server" + ar_err.getMessage());
+  }
+});
