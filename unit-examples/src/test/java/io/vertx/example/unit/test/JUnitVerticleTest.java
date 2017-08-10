17b2c37106cfef98fafd7a26c7922737e1ebd3c7
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-08-11 01:07:59 +0200

    JUnitVerticleTest - test Verticle with JUnit

---
+++ unit-examples/src/test/java/io/vertx/example/unit/test/JUnitVerticleTest.java
@@ -0,0 +1,59 @@
+package io.vertx.example.unit.test;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.json.JsonObject;
+import io.vertx.example.unit.HelloVerticle;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/*
+ * Example of an asynchronous JUnit test for a Verticle.
+ */
+@RunWith(VertxUnitRunner.class)
+public class JUnitVerticleTest {
+
+  Vertx vertx;
+  int port;
+
+  @Before
+  public void before(TestContext context) throws IOException {
+    ServerSocket socket = new ServerSocket(0);
+    port = socket.getLocalPort();
+    socket.close();
+
+    DeploymentOptions options = new DeploymentOptions()
+        .setConfig(new JsonObject().put("http.port", port));
+
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(HelloVerticle.class.getName(), options, context.asyncAssertSuccess());
+  }
+
+  @After
+  public void after(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void canGetHello(TestContext context) {
+    Async async = context.async();
+    HttpClient client = vertx.createHttpClient();
+    client.getNow(port, "localhost", "/", response -> {
+      response.bodyHandler(body -> {
+        context.assertEquals("Hello!", body.toString());
+        client.close();
+        async.complete();
+      });
+    });
+  }
+}
