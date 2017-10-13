fd7d025c1c60b6cff4aa2f987ea42e4c1ff46e45
Julien Viet <julien@julienviet.com>
2017-10-13 23:41:44 +0200

    Update to latest staging repo

--- core-examples/src/main/java/io/vertx/example/core/http/simple/Server.java
+++ core-examples/src/main/java/io/vertx/example/core/http/simple/Server.java
@@ -17,6 +17,6 @@ public class Server extends AbstractVerticle {
   public void start() throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
       req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>");
-    }).listen(8080);
+    }).listen(8081);
   }
 }
