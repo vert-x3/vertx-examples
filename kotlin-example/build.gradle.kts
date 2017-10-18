f9cf6720dd5a557cc6d5096b5ce61603f99d00a2
Nick Apperley <napperley@gmail.com>
2017-10-18 14:11:24 +1300

    Update Gradle to 4.2.1.

--- kotlin-example/build.gradle.kts
+++ kotlin-example/build.gradle.kts
@@ -1,21 +1,22 @@
+@file:Suppress("PropertyName")
+
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URI
 
 val MAIN_VERTICLE = "io.vertx.example.MainVerticle"
 val MAIN_CLASS = "io.vertx.core.Launcher"
-val KOTLIN_VER = "1.1.3"
 val VERTX_VER = "3.4.2"
 val SHADOW_VER = "2.0.1"
 
-if (!JavaVersion.current().isJava8Compatible) {
-    IllegalStateException("Java 8 required!")
-}
+if (!JavaVersion.current().isJava8Compatible) IllegalStateException("Java 8 required!")
 
 // Section for setting up repositories and dependencies for plugins.
 buildscript {
     // Should be able to setup extra project metadata here however it is limited by scope for some strange reason.
     // Can setup metadata by doing the following, eg: extra["kotlin-ver"] = "1.1.3"
+    extra["kotlin-ver"] = "1.1.51"
+
     repositories {
         jcenter()
         mavenCentral()
@@ -29,6 +30,8 @@ buildscript {
     }
 }
 
+val KOTLIN_VER = "${extra["kotlin-ver"]}"
+
 apply {
     plugin("com.github.johnrengelman.shadow")
 }
@@ -37,7 +40,9 @@ apply {
 plugins {
     java
     application
-    kotlin("jvm")
+    // Apply the Kotlin JVM Gradle plugin. Version has to be specified manually due to a Gradle limitation.
+    // Refer to the documentation on "plugins" block limitations: https://docs.gradle.org/current/userguide/plugins.html#plugins_dsl_limitations
+    kotlin(module = "jvm", version = "1.1.51")
 }
 
 // Setup repositories for libraries/frameworks.
@@ -53,8 +58,7 @@ repositories {
 dependencies {
     compile("io.vertx:vertx-core:$VERTX_VER")
     compile("io.vertx:vertx-web:$VERTX_VER")
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$KOTLIN_VER")
-    compile("org.jetbrains.kotlin:kotlin-runtime:$KOTLIN_VER")
+    compile(kotlin(module = "stdlib-jre8", version = KOTLIN_VER))
 }
 
 application { mainClassName = MAIN_CLASS }
@@ -94,10 +98,6 @@ tasks {
         mergeServiceFiles {
             include("META-INF/services/io.vertx.core.spi.VerticleFactory")
         }
-    }
-
-    task(name = "wrapper", type = Wrapper::class) {
-        gradleVersion = "4.0"
     }
 
     // Heroku relies on the 'stage' task to deploy.
