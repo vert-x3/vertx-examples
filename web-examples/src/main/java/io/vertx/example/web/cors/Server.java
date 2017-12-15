e75f2a63e33dee4df29219377f21b75d12543c05
Giacomo Venturini <giacomo.venturini2@studio.unibo.it>
2017-12-15 19:58:37 +0100

    modified the code style to respect Vert.x code style guidelines

--- web-examples/src/main/java/io/vertx/example/web/cors/Server.java
+++ web-examples/src/main/java/io/vertx/example/web/cors/Server.java
@@ -19,68 +19,65 @@ import java.util.Set;
  */
 public class Server extends AbstractVerticle {
 
-    // Convenience method so you can run it in your IDE
-    public static void main(String[] args) {
-        Runner.runExample(Server.class);
-    }
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Server.class);
+  }
 
-    @Override
-    public void start() throws Exception {
+  @Override
+  public void start() throws Exception {
 
-        Router router = Router.router(vertx);
+    Router router = Router.router(vertx);
 
-        Set<String> allowedHeaders = new HashSet<>();
-        allowedHeaders.add("x-requested-with");
-        allowedHeaders.add("Access-Control-Allow-Origin");
-        allowedHeaders.add("origin");
-        allowedHeaders.add("Content-Type");
-        allowedHeaders.add("accept");
-        allowedHeaders.add("X-PINGARUNER");
+    Set<String> allowedHeaders = new HashSet<>();
+    allowedHeaders.add("x-requested-with");
+    allowedHeaders.add("Access-Control-Allow-Origin");
+    allowedHeaders.add("origin");
+    allowedHeaders.add("Content-Type");
+    allowedHeaders.add("accept");
+    allowedHeaders.add("X-PINGARUNER");
 
-        Set<HttpMethod> allowedMethods = new HashSet<>();
-        allowedMethods.add(HttpMethod.GET);
-        allowedMethods.add(HttpMethod.POST);
-        allowedMethods.add(HttpMethod.OPTIONS);
-        /* 
-            these methods aren't necessary for this sample, 
-            but you may need them for your projects
-        */
-        allowedMethods.add(HttpMethod.DELETE);
-        allowedMethods.add(HttpMethod.PATCH);
-        allowedMethods.add(HttpMethod.PUT);
-        
+    Set<HttpMethod> allowedMethods = new HashSet<>();
+    allowedMethods.add(HttpMethod.GET);
+    allowedMethods.add(HttpMethod.POST);
+    allowedMethods.add(HttpMethod.OPTIONS);
+    /*
+     * these methods aren't necessary for this sample, 
+     * but you may need them for your projects
+     */
+    allowedMethods.add(HttpMethod.DELETE);
+    allowedMethods.add(HttpMethod.PATCH);
+    allowedMethods.add(HttpMethod.PUT);
 
-        router.route().handler(CorsHandler.create("*")
-            .allowedHeaders(allowedHeaders)
-            .allowedMethods(allowedMethods));
+    router.route().handler(CorsHandler.create("*").allowedHeaders(allowedHeaders).allowedMethods(allowedMethods));
 
-        router.get("/access-control-with-get").handler(ctx -> {
-            HttpServerResponse httpServerResponse = ctx.response();
-            httpServerResponse.setChunked(true);
-            MultiMap headers = ctx.request().headers();
-            for (String key : headers.names()){
-                httpServerResponse.write(key + ": ");
-                httpServerResponse.write(headers.get(key));
-                httpServerResponse.write("<br>");
-            }
-            httpServerResponse.putHeader("Content-Type", "application/text").end("Success");
-        });
+    router.get("/access-control-with-get").handler(ctx -> {
+      HttpServerResponse httpServerResponse = ctx.response();
+      httpServerResponse.setChunked(true);
+      MultiMap headers = ctx.request().headers();
+      for (String key : headers.names()) {
+        httpServerResponse.write(key + ": ");
+        httpServerResponse.write(headers.get(key));
+        httpServerResponse.write("<br>");
+      }
+      httpServerResponse.putHeader("Content-Type", "application/text").end("Success");
+    });
 
-        router.post("/access-control-with-post-preflight").handler(ctx -> {
-            HttpServerResponse httpServerResponse = ctx.response();
-            httpServerResponse.setChunked(true);
-            MultiMap headers = ctx.request().headers();
-            for (String key : headers.names()) {
-              httpServerResponse.write(key + ": ");
-              httpServerResponse.write(headers.get(key));
-              httpServerResponse.write("<br>");
-            }
-            httpServerResponse.putHeader("Content-Type", "application/text").end("Success");
-        });
+    router.post("/access-control-with-post-preflight").handler(ctx -> {
+      HttpServerResponse httpServerResponse = ctx.response();
+      httpServerResponse.setChunked(true);
+      MultiMap headers = ctx.request().headers();
+      for (String key : headers.names()) {
+        httpServerResponse.write(key + ": ");
+        httpServerResponse.write(headers.get(key));
+        httpServerResponse.write("<br>");
+      }
+      httpServerResponse.putHeader("Content-Type", "application/text").end("Success");
+    });
 
-        // Serve the static resources
-        router.route().handler(StaticHandler.create());
+    // Serve the static resources
+    router.route().handler(StaticHandler.create());
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-    }
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
 }
