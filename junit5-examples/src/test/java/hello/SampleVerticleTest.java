48e57c888b0901fd0e194ea52cdae45b3649c148
Julien Ponge <julien.ponge@gmail.com>
2018-01-17 21:23:16 +0100

    More examples
    Includes Before/After/Nested

--- junit5-examples/src/test/java/hello/SampleVerticleTest.java
+++ junit5-examples/src/test/java/hello/SampleVerticleTest.java
@@ -17,15 +17,14 @@
 package hello;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -79,5 +78,48 @@ class SampleVerticleTest {
           }));
       }
     }));
+  }
+
+  @DisplayName("➡️ A nested test with customized lifecycle")
+  @Nested
+  class CustomLifecycleTest {
+
+    Vertx vertx;
+
+    @BeforeEach
+    void prepare() {
+      vertx = Vertx.vertx(new VertxOptions()
+        .setMaxEventLoopExecuteTime(1000)
+        .setPreferNativeTransport(true)
+        .setFileResolverCachingEnabled(true));
+    }
+
+    @Test
+    @DisplayName("⬆️ Deploy SampleVerticle")
+    void deploySampleVerticle(VertxTestContext testContext) {
+      vertx.deployVerticle(new SampleVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    }
+
+    @Test
+    @DisplayName("🛂 Make a HTTP client request to SampleVerticle")
+    void httpRequest(VertxTestContext testContext) {
+      WebClient webClient = WebClient.create(vertx);
+      vertx.deployVerticle(new SampleVerticle(), testContext.succeeding(id -> {
+        webClient.get(11981, "localhost", "/yo")
+          .as(BodyCodec.string())
+          .send(testContext.succeeding(resp -> {
+            testContext.verify(() -> {
+              assertThat(resp.statusCode()).isEqualTo(200);
+              assertThat(resp.body()).contains("Yo!");
+              testContext.completeNow();
+            });
+          }));
+      }));
+    }
+
+    @AfterEach
+    void cleanup() {
+      vertx.close();
+    }
   }
 }
