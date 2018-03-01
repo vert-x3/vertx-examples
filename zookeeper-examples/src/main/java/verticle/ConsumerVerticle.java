4ce96ac16109f8f357d4834d71497f19e2c62506
Mohamed Ibrahim <mis1494@gmail.com>
2018-03-01 18:54:39 +0200

    Update ConsumerVerticle.java

--- zookeeper-examples/src/main/java/verticle/ConsumerVerticle.java
+++ zookeeper-examples/src/main/java/verticle/ConsumerVerticle.java
@@ -4,12 +4,10 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
 
 public class ConsumerVerticle extends AbstractVerticle {
 
-    private String verticleAddress = "Weather";
+    private String verticleAddress = "Consumer";
     private EventBus eventBus;
 
     @Override
@@ -26,16 +24,9 @@ public class ConsumerVerticle extends AbstractVerticle {
         MessageConsumer<JsonObject> messageConsumer = eventBus.consumer(verticleAddress);
         messageConsumer.handler(message -> {
             JsonObject jsonMessage = message.body();
-            System.out.println(jsonMessage.getValue("message_from_HTTP_request"));
-            WebClient webClient = WebClient.create(vertx);
-            webClient.get(443, "jsonplaceholder.typicode.com", "/posts/1").ssl(true).as(BodyCodec.jsonObject())
-                    .send(httpResponseAsyncResult -> {
-                        if(httpResponseAsyncResult.succeeded()) {
-                            JsonObject jsonReply = httpResponseAsyncResult.result().body();
-                            jsonReply.put("reply", "how interesting!");
-                            message.reply(jsonReply);
-                        }
-                    });
+            System.out.println(jsonMessage.getValue("message_from_sender_verticle"));
+            JsonObject jsonReply = new JsonObject().put("reply", "how interesting!");
+            message.reply(jsonReply);
         });
     }
 }
