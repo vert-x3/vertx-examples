167eb1cecac13ede047f55fcfbd3196c703bcb1c
Julien Ponge <julien.ponge@gmail.com>
2018-12-19 14:54:34 +0100

    Dropping a meaningless example

--- gradle-verticles/gradle-verticle-groovy-compiled/src/main/groovy/MyGroovyVerticle.groovy
+++
@@ -1,3 +0,0 @@
-vertx.createHttpServer().requestHandler({ req ->
-    req.response().putHeader("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>")
-}).listen(8080)
