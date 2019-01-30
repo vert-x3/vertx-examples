70b9b07e7a739a43ca50e149e67532a0f3a944a5
Julien Viet <julien@julienviet.com>
2019-01-30 19:48:46 +0100

    Update version + staging

--- osgi-examples/src/test/java/io/vertx/example/osgi/it/VertxServiceIT.java
+++ osgi-examples/src/test/java/io/vertx/example/osgi/it/VertxServiceIT.java
@@ -87,7 +87,7 @@ public class VertxServiceIT {
       mavenBundle("org.apache.felix", "org.apache.felix.ipojo").versionAsInProject(),
       mavenBundle("commons-io", "commons-io").versionAsInProject(),
 
-      bundle("file:target/osgi-examples-" + System.getProperty("project.version", "3.6.2") + ".jar"),
+      bundle("file:target/osgi-examples-" + System.getProperty("project.version", "3.6.3") + ".jar"),
 
       junitBundles()
     );
