73a27d91f5816e870b849b35c1bf4df089d78de7
Tobias Doerner <tobias.doerner@mailbox.org>
2019-03-12 22:58:54 +0100

    Improving vertx-heroku example
    Updated version of the heroku-maven-plugin.
    
    Using the vertx config retriever to retrieve the system properties and the config vars defined in heroku application settings.

--- heroku-example/src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ heroku-example/src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -1,15 +1,63 @@
 package io.vertx.example;
 
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 
 public class HelloWorldVerticle extends AbstractVerticle {
 
+  private ConfigRetriever configRetriever;
+  private JsonObject config;
+
   @Override
-  public void start() throws Exception {
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!"))
-    .listen(
-        Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"));
+  public void start(Future<Void> startFuture) throws Exception {
+    retrieveConfig().setHandler(ar -> {
+      if (ar.succeeded()) {
+        vertx.createHttpServer()
+          .requestHandler(req -> req.response().end("Hello World!"))
+          .listen(config.getInteger("http.port"), config.getString("http.address", "0.0.0.0"));
+        startFuture.complete();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+
+  @Override
+  public void stop() throws Exception {
+    configRetriever.close();
+    super.stop();
+  }
+
+  private Future<Void> retrieveConfig() {
+    final Future<Void> future = Future.future();
+
+    // Option for reading the Config Vars which are defined in the settings of the application on Heroku
+    final ConfigStoreOptions configStoreEnvOptions = new ConfigStoreOptions()
+      .setType("env")
+      .setOptional(true);
+
+    final ConfigStoreOptions configStoreSysOptions = new ConfigStoreOptions()
+      .setType("sys")
+      .setOptional(true);
+
+    final ConfigRetrieverOptions configRetrieverOptions = new ConfigRetrieverOptions()
+      .addStore(configStoreEnvOptions)
+      .addStore(configStoreSysOptions);
+
+    configRetriever = ConfigRetriever.create(vertx, configRetrieverOptions);
+    configRetriever.getConfig(ar -> {
+      if (ar.succeeded()) {
+        config = ar.result();
+        future.complete();
+      } else {
+        future.fail(ar.cause());
+      }
+    });
+
+    return future;
   }
 }
-
