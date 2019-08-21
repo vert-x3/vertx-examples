de9ab5f25487d5a6d8d8bfecda1c3e5194c4b566
Julien Viet <julien@julienviet.com>
2019-08-21 17:05:24 +0200

    Upgrade to 3.8.1

--- mail-examples/src/main/groovy/io/vertx/example/mail/mail_headers.groovy
+++ mail-examples/src/main/groovy/io/vertx/example/mail/mail_headers.groovy
@@ -13,7 +13,7 @@ def email = [
   from:"user1@example.com",
   to:["user2@example.com", "user3@example.com", "user4@example.com"],
   headers:[
-    'X-Mailer':"Vert.x Mail-Client 3.8.0",
+    'X-Mailer':"Vert.x Mail-Client 3.8.1",
     'Message-ID':"12345@example.com",
     'Reply-To':"reply@example.com",
     Received:[
