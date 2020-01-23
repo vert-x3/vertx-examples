4ca8cdc84b7a146bcd80a1d8779038d0f6d35391
Julien Viet <julien@julienviet.com>
2020-01-23 08:34:56 +0100

    Update staging repository

--- web-examples/src/main/kotlin/io/vertx/example/web/authjdbc/Server.kt
+++ web-examples/src/main/kotlin/io/vertx/example/web/authjdbc/Server.kt
@@ -24,7 +24,7 @@ class Server : io.vertx.core.AbstractVerticle()  {
     executeStatement("create table user_roles (username varchar(255), role varchar(255));")
     executeStatement("create table roles_perms (role varchar(255), perm varchar(255));")
 
-    executeStatement("insert into user values ('tim', 'EC0D6302E35B7E792DF9DA4A5FE0DB3B90FCAB65A6215215771BF96D498A01DA8234769E1CE8269A105E9112F374FDAB2158E7DA58CDC1348A732351C38E12A0', 'C59EB438D1E24CACA2B1A48BC129348589D49303858E493FBE906A9158B7D5DC');")
+    executeStatement("insert into user values ('tim', 'EC0D6302E35B7E792DF9DA4A5FE0DB3B90FCAB65A6215215771BF96D498A01DA8234769E1CE8269A105E9112F374FDAB2158E7DA58CDC1348A732351C38E12A0', 'C59EB438D1E24CACA2B1A48BC129348589D49303863E493FBE906A9158B7D5DC');")
     executeStatement("insert into user_roles values ('tim', 'dev');")
     executeStatement("insert into user_roles values ('tim', 'admin');")
     executeStatement("insert into roles_perms values ('dev', 'commit_code');")
