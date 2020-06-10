daec420f68fd8b975df29696d7dfbdbac5429760
Thomas Segismont <tsegismo@redhat.com>
2020-06-10 15:40:33 +0200

    Added service proxy generated files

---
+++ service-proxy-examples/service-provider/src/main/generated/io/vertx/examples/service/ProcessorServiceVertxEBProxy.java
@@ -0,0 +1,84 @@
+/*
+* Copyright 2014 Red Hat, Inc.
+*
+* Red Hat licenses this file to you under the Apache License, version 2.0
+* (the "License"); you may not use this file except in compliance with the
+* License. You may obtain a copy of the License at:
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+package io.vertx.examples.service;
+
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+import io.vertx.serviceproxy.ServiceException;
+import io.vertx.serviceproxy.ServiceExceptionMessageCodec;
+import io.vertx.serviceproxy.ProxyUtils;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.examples.service.ProcessorService;
+/*
+  Generated Proxy code - DO NOT EDIT
+  @author Roger the Robot
+*/
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class ProcessorServiceVertxEBProxy implements ProcessorService {
+  private Vertx _vertx;
+  private String _address;
+  private DeliveryOptions _options;
+  private boolean closed;
+
+  public ProcessorServiceVertxEBProxy(Vertx vertx, String address) {
+    this(vertx, address, null);
+  }
+
+  public ProcessorServiceVertxEBProxy(Vertx vertx, String address, DeliveryOptions options) {
+    this._vertx = vertx;
+    this._address = address;
+    this._options = options;
+    try {
+      this._vertx.eventBus().registerDefaultCodec(ServiceException.class, new ServiceExceptionMessageCodec());
+    } catch (IllegalStateException ex) {
+    }
+  }
+
+  @Override
+  public void process(JsonObject document, Handler<AsyncResult<JsonObject>> resultHandler){
+    if (closed) {
+      resultHandler.handle(Future.failedFuture(new IllegalStateException("Proxy is closed")));
+      return;
+    }
+    JsonObject _json = new JsonObject();
+    _json.put("document", document);
+
+    DeliveryOptions _deliveryOptions = (_options != null) ? new DeliveryOptions(_options) : new DeliveryOptions();
+    _deliveryOptions.addHeader("action", "process");
+    _vertx.eventBus().<JsonObject>request(_address, _json, _deliveryOptions, res -> {
+      if (res.failed()) {
+        resultHandler.handle(Future.failedFuture(res.cause()));
+      } else {
+        resultHandler.handle(Future.succeededFuture(res.result().body()));
+      }
+    });
+  }
+}
---
+++ service-proxy-examples/service-provider/src/main/generated/io/vertx/examples/service/rxjava/ProcessorService.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.vertx.examples.service.rxjava;
+
+import rx.Observable;
+import rx.Single;
+import io.vertx.rx.java.RxHelper;
+import io.vertx.rx.java.WriteStreamSubscriber;
+import io.vertx.rx.java.SingleOnSubscribeAdapter;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import io.vertx.core.Handler;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import io.vertx.lang.rx.RxGen;
+import io.vertx.lang.rx.TypeArg;
+import io.vertx.lang.rx.MappingIterator;
+
+/**
+ * The service interface.
+ *
+ * <p/>
+ * NOTE: This class has been automatically generated from the {@link io.vertx.examples.service.ProcessorService original} non RX-ified interface using Vert.x codegen.
+ */
+
+@RxGen(io.vertx.examples.service.ProcessorService.class)
+public class ProcessorService {
+
+  @Override
+  public String toString() {
+    return delegate.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ProcessorService that = (ProcessorService) o;
+    return delegate.equals(that.delegate);
+  }
+  
+  @Override
+  public int hashCode() {
+    return delegate.hashCode();
+  }
+
+  public static final TypeArg<ProcessorService> __TYPE_ARG = new TypeArg<>(    obj -> new ProcessorService((io.vertx.examples.service.ProcessorService) obj),
+    ProcessorService::getDelegate
+  );
+
+  private final io.vertx.examples.service.ProcessorService delegate;
+  
+  public ProcessorService(io.vertx.examples.service.ProcessorService delegate) {
+    this.delegate = delegate;
+  }
+
+  public ProcessorService(Object delegate) {
+    this.delegate = (io.vertx.examples.service.ProcessorService)delegate;
+  }
+
+  public io.vertx.examples.service.ProcessorService getDelegate() {
+    return delegate;
+  }
+
+  public static io.vertx.examples.service.rxjava.ProcessorService create(io.vertx.rxjava.core.Vertx vertx) { 
+    io.vertx.examples.service.rxjava.ProcessorService ret = io.vertx.examples.service.rxjava.ProcessorService.newInstance((io.vertx.examples.service.ProcessorService)io.vertx.examples.service.ProcessorService.create(vertx.getDelegate()));
+    return ret;
+  }
+
+  public static io.vertx.examples.service.rxjava.ProcessorService createProxy(io.vertx.rxjava.core.Vertx vertx, String address) { 
+    io.vertx.examples.service.rxjava.ProcessorService ret = io.vertx.examples.service.rxjava.ProcessorService.newInstance((io.vertx.examples.service.ProcessorService)io.vertx.examples.service.ProcessorService.createProxy(vertx.getDelegate(), address));
+    return ret;
+  }
+
+  public void process(JsonObject document, Handler<AsyncResult<JsonObject>> resultHandler) { 
+    delegate.process(document, resultHandler);
+  }
+
+  public void process(JsonObject document) {
+    process(document, ar -> { });
+  }
+
+    public Single<JsonObject> rxProcess(JsonObject document) { 
+    return Single.create(new SingleOnSubscribeAdapter<>(fut -> {
+      process(document, fut);
+    }));
+  }
+
+  public static final int NO_NAME_ERROR = io.vertx.examples.service.ProcessorService.NO_NAME_ERROR;
+  public static final int BAD_NAME_ERROR = io.vertx.examples.service.ProcessorService.BAD_NAME_ERROR;
+  public static ProcessorService newInstance(io.vertx.examples.service.ProcessorService arg) {
+    return arg != null ? new ProcessorService(arg) : null;
+  }
+
+}
