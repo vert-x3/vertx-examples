a54c2252c11f15dfbc077be43adca74102f6cad2
Thomas Segismont <tsegismo@redhat.com>
2020-06-18 16:05:49 +0200

    Remove Gradle Verticle JS and Ruby

--- gradle-verticles/gradle-verticle-ruby/src/main/ruby/MyRubyVerticle.rb
+++
@@ -1,3 +0,0 @@
-$vertx.create_http_server().request_handler() { |req|
-  req.response().put_header("content-type", "text/html").end("<html><body><h1>Hello from vert.x!</h1></body></html>")
-}.listen(8080)
