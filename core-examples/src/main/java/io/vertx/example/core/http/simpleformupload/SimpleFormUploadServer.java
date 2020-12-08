ce46fcdffd0e898dab2942b8efc46264750e7171
Julien Viet <julien@julienviet.com>
2020-12-08 09:32:46 +0100

    Fix HTTP server file upload stream example - close #406

--- core-examples/src/main/java/io/vertx/example/core/http/simpleformupload/SimpleFormUploadServer.java
+++ core-examples/src/main/java/io/vertx/example/core/http/simpleformupload/SimpleFormUploadServer.java
@@ -25,21 +25,17 @@ public class SimpleFormUploadServer extends AbstractVerticle {
       } else if (req.uri().startsWith("/form")) {
         req.setExpectMultipart(true);
         req.uploadHandler(upload -> {
-          upload.exceptionHandler(cause -> {
-            req.response().setChunked(true).end("Upload failed");
-          });
-
-          upload.endHandler(v -> {
-            req.response().setChunked(true).end("Successfully uploaded to " + upload.filename());
-          });
           // FIXME - Potential security exploit! In a real system you must check this filename
           // to make sure you're not saving to a place where you don't want!
           // Or better still, just use Vert.x-Web which controls the upload area.
-          upload.streamToFileSystem(upload.filename());
+          upload.streamToFileSystem(upload.filename())
+            .onSuccess(v -> req.response().end("Successfully uploaded to " + upload.filename()))
+            .onFailure(err -> req.response().end("Upload failed"));
         });
       } else {
-        req.response().setStatusCode(404);
-        req.response().end();
+        req.response()
+          .setStatusCode(404)
+          .end();
       }
     }).listen(8080);
 
