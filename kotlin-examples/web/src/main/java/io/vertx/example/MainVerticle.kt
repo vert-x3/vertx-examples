3a6b5a245858968dc5d7f3ca9ae34fa17f4572fd
Julien Ponge <jponge@redhat.com>
2021-05-31 20:37:25 +0200

    Fix and upgrade the web example

--- kotlin-examples/web/src/main/java/io/vertx/example/MainVerticle.kt
+++ kotlin-examples/web/src/main/java/io/vertx/example/MainVerticle.kt
@@ -1,70 +1,67 @@
 package io.vertx.example
 
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.Future
 import io.vertx.core.Handler
+import io.vertx.core.Promise
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.json.Json
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 
-@Suppress("unused")
 class MainVerticle : AbstractVerticle() {
 
-    override fun start(startFuture: Future<Void>) {
-        val router = createRouter()
+  override fun start(promise: Promise<Void>) {
+    val router = createRouter()
 
-        vertx.createHttpServer()
-                .requestHandler(router)
-                .listen(config().getInteger("http.port", 8080)) { result ->
-                    if (result.succeeded()) {
-                        startFuture.complete()
-                    } else {
-                        startFuture.fail(result.cause())
-                    }
-                }
-    }
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(config().getInteger("http.port", 8080))
+      .onSuccess { promise.complete() }
+      .onFailure { promise.fail(it) }
+  }
 
-    private fun createRouter() = Router.router(vertx).apply {
-        get("/").handler(handlerRoot)
-        get("/islands").handler(handlerIslands)
-        get("/countries").handler(handlerCountries)
-    }
+  private fun createRouter() = Router.router(vertx).apply {
+    get("/").handler(handlerRoot)
+    get("/islands").handler(handlerIslands)
+    get("/countries").handler(handlerCountries)
+  }
 
-    //
-    // Handlers
+  //
+  // Handlers
 
-    val handlerRoot = Handler<RoutingContext> { req ->
-        req.response().end("Welcome!")
-    }
+  val handlerRoot = Handler<RoutingContext> { req ->
+    req.response().end("Welcome!")
+  }
 
-    val handlerIslands = Handler<RoutingContext> { req ->
-        req.response().endWithJson(MOCK_ISLANDS)
-    }
+  val handlerIslands = Handler<RoutingContext> { req ->
+    req.response().endWithJson(MOCK_ISLANDS)
+  }
 
-    val handlerCountries = Handler<RoutingContext> { req ->
-        req.response().endWithJson(MOCK_ISLANDS.map { it.country }.distinct().sortedBy { it.code })
-    }
+  val handlerCountries = Handler<RoutingContext> { req ->
+    req.response().endWithJson(MOCK_ISLANDS.map { it.country }.distinct().sortedBy { it.code })
+  }
 
-    //
-    // Mock data
+  //
+  // Mock data
 
-    private val MOCK_ISLANDS by lazy {
-        listOf(
-                Island("Kotlin", Country("Russia", "RU")),
-                Island("Stewart Island", Country("New Zealand", "NZ")),
-                Island("Cockatoo Island", Country("Australia", "AU")),
-                Island("Tasmania", Country("Australia", "AU"))
-        )
-    }
+  private val MOCK_ISLANDS by lazy {
+    listOf(
+      Island("Kotlin", Country("Russia", "RU")),
+      Island("Stewart Island", Country("New Zealand", "NZ")),
+      Island("Cockatoo Island", Country("Australia", "AU")),
+      Island("Tasmania", Country("Australia", "AU"))
+    )
+  }
 
-    //
-    // Utilities
+  //
+  // Utilities
 
-    /**
-     * Extension to the HTTP response to output JSON objects.
-     */
-    fun HttpServerResponse.endWithJson(obj: Any) {
-        this.putHeader("Content-Type", "application/json; charset=utf-8").end(Json.encodePrettily(obj))
-    }
+  /**
+   * Extension to the HTTP response to output JSON objects.
+   */
+  fun HttpServerResponse.endWithJson(obj: Any) {
+    this
+      .putHeader("Content-Type", "application/json; charset=utf-8")
+      .end(Json.encodePrettily(obj))
+  }
 }
