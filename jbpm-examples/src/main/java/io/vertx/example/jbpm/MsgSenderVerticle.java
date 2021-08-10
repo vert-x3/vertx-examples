19beb7f189049f052a139926dbde90ab523812c7
Prasanth Nair <prasanth.nair@aexp.com>
2021-08-10 11:25:18 -0700

    fixes

--- jbpm-examples/src/main/java/io/vertx/example/jbpm/MsgSenderVerticle.java
+++ jbpm-examples/src/main/java/io/vertx/example/jbpm/MsgSenderVerticle.java
@@ -1,11 +1,8 @@
 package io.vertx.example.jbpm;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
 
 public class MsgSenderVerticle extends AbstractVerticle {
 
