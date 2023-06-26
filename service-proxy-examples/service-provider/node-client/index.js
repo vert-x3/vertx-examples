8245c066da2e28234259cce9771dc686740484fe
Thomas Segismont <tsegismont@gmail.com>
2023-06-26 16:52:35 +0200

    Update Service Proxy examples
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- service-proxy-examples/service-provider/node-client/index.js
+++ service-proxy-examples/service-provider/node-client/index.js
@@ -1,12 +1,12 @@
-var EventBus = require('vertx3-eventbus-client');
-var ProcessorService = require('../src/main/resources/vertx-processor-sample-js/processor_service-proxy');
+var EventBus = require('@vertx/eventbus-bridge-client.js');
+var ProcessorService = require('../src/main/java/io/vertx/examples/service/webroot/processor_service-proxy.js');
 
 var eb = new EventBus('http://localhost:8080/eventbus/');
 
 eb.onopen = function () {
   var processorService = new ProcessorService(eb, "vertx.processor");
 
-  processorService.process({foo: 'bar'}, function (err, res) {
+  processorService.process({name: 'foo', value: 'bar'}, function (err, res) {
     if (err) {
       console.error(err);
       return;
