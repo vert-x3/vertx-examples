a2da5a17d0a46ee94008e4f824e199967f318e91
Julien Viet <julien@julienviet.com>
2024-10-19 13:57:12 +0200

    Improve Mongo example

--- mongo-examples/src/main/java/io/vertx/examples/mongo/MongoClientVerticle.java
+++ mongo-examples/src/main/java/io/vertx/examples/mongo/MongoClientVerticle.java
@@ -1,19 +1,22 @@
 package io.vertx.examples.mongo;
 
-import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.VerticleBase;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.launcher.application.VertxApplication;
 
-public class MongoClientVerticle extends AbstractVerticle {
+public class MongoClientVerticle extends VerticleBase {
 
   public static void main(String[] args) {
     VertxApplication.main(new String[]{MongoClientVerticle.class.getName()});
   }
 
+  private MongoClient mongoClient;
+
   @Override
-  public void start() throws Exception {
+  public Future<?> start() throws Exception {
 
     JsonObject config = Vertx.currentContext().config();
 
@@ -30,11 +33,11 @@ public class MongoClientVerticle extends AbstractVerticle {
       .put("connection_string", uri)
       .put("db_name", db);
 
-    MongoClient mongoClient = MongoClient.createShared(vertx, mongoconfig);
+    mongoClient = MongoClient.createShared(vertx, mongoconfig);
 
     JsonObject product1 = new JsonObject().put("itemId", "12345").put("name", "Cooler").put("price", "100.0");
 
-    mongoClient.save("products", product1)
+    return mongoClient.save("products", product1)
       .compose(id -> {
         System.out.println("Inserted id: " + id);
         return mongoClient.find("products", new JsonObject().put("itemId", "12345"));
@@ -43,12 +46,8 @@ public class MongoClientVerticle extends AbstractVerticle {
         System.out.println("Name is " + res.get(0).getString("name"));
         return mongoClient.removeDocument("products", new JsonObject().put("itemId", "12345"));
       })
-      .onComplete(ar -> {
-        if (ar.succeeded()) {
-          System.out.println("Product removed ");
-        } else {
-          ar.cause().printStackTrace();
-        }
+      .onSuccess(res -> {
+        System.out.println("Product removed ");
       });
   }
 }
