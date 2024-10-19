59068570f4054f7a908e4f3854edd332ad87a888
Julien Viet <julien@julienviet.com>
2024-10-19 14:06:08 +0200

    Redis client example improvements

--- redis-examples/src/main/java/io/vertx/examples/redis/RedisClientVerticle.java
+++ redis-examples/src/main/java/io/vertx/examples/redis/RedisClientVerticle.java
@@ -1,7 +1,7 @@
 package io.vertx.examples.redis;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.Future;
+import io.vertx.core.VerticleBase;
 import io.vertx.launcher.application.VertxApplication;
 import io.vertx.redis.client.Redis;
 import io.vertx.redis.client.RedisAPI;
@@ -12,26 +12,27 @@ import java.util.Arrays;
 /**
  * A verticle setting and reading a value in Redis.
  */
-public class RedisClientVerticle extends AbstractVerticle {
-
+public class RedisClientVerticle extends VerticleBase {
 
   public static void main(String[] args) {
     VertxApplication.main(new String[]{RedisClientVerticle.class.getName()});
   }
 
+  private RedisAPI redis;
+
   @Override
-  public void start() {
+  public Future<?> start() {
     // If a config file is set, read the host and port.
-    String host = Vertx.currentContext().config().getString("host");
+    String host = config().getString("host");
     if (host == null) {
       host = "127.0.0.1";
     }
 
     // Create the redis client
     Redis client = Redis.createClient(vertx, new RedisOptions().addConnectionString(host));
-    RedisAPI redis = RedisAPI.api(client);
+    redis = RedisAPI.api(client);
 
-    client.connect()
+    return client.connect()
       .compose(conn ->
         redis.set(Arrays.asList("key", "value"))
           .compose(v -> {
@@ -40,9 +41,6 @@ public class RedisClientVerticle extends AbstractVerticle {
           }))
       .onSuccess(result -> {
         System.out.println("Retrieved value: " + result);
-      })
-      .onFailure(err -> {
-        System.out.println("Connection or Operation Failed " + err);
       });
   }
 }
